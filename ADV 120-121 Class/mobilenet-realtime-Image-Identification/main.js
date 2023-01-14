4d9b0cac980a4b90e7e5068855f3f391df961212
Python Tinkerer <87096212+ProOfPython@users.noreply.github.com>
2023-01-13 16:54:49 -0800

    Add files via upload

--- ADV 120-121 Class/mobilenet-realtime-Image-Identification/main.js
+++ ADV 120-121 Class/mobilenet-realtime-Image-Identification/main.js
@@ -1,11 +1,11 @@
-var preRes = ''
-
+const MAX_TICKS = 100
+var tick = MAX_TICKS - 1
 function setup() {
 	canvas = createCanvas(300, 300);
 	canvas.center();
 	video = createCapture(VIDEO);
 	video.hide();
-	classifier = ml5.imageClassifier('MobilaNet', modelLoaded())
+	classifier = ml5.imageClassifier('MobileNet', modelLoaded())
 }
 
 function modelLoaded(){
@@ -14,16 +14,19 @@ function modelLoaded(){
 
 function draw(){
 	image(video, 0, 0, 300, 300)
-	classifier.classify(video, gotResult())
+	classifier.classify(video, gotResult)
 }
 
 function gotResult(error, results){
 	if (error){
 		consele.log(error)
-	} else if( (results[0].confidence > 0.5) && (preRes != results[0].label) ) {
-		console.log(results)
-		preRes = results[0].label
-		document.getElementById('resObj').innerHTML = results[0].label
-		document.getElementById('resAcc').innerHTML = results[0].confidence.toFixed(2) + "%"
+	} else {
+		tick += 1
+		if (tick == MAX_TICKS){ 
+			console.log(results)
+			document.getElementById('resObj').innerHTML = results[0].label
+			document.getElementById('resAcc').innerHTML = results[0].confidence.toFixed(2) * 100 + "%"
+			tick = 0
+		}
 	}
 }
